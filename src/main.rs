bf9c2dd09821b1e389e1bf0511cadf2911714aa5
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-07-28 14:09:22 +0300

    Cast guess to u32 & match it with secret_number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
-fn main() {
-    print!("Guess the number!\nPlease input your guess\n");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+fn main()
+{
+     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_number}");
+    println!("Guess the number!\nPlease input your guess");
     let mut guess = String::new();
     io::stdin()
 	.read_line(&mut guess)
 	.expect("Failed to read line");
-    print!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {guess}");
+    match guess.cmp(&secret_number)
+    {
+	Ordering::Less => println!("Too small!"),
+	Ordering::Greater => println!("Too Big!"),
+	Ordering::Equal => println!("You win!")
+    }
 }
