98578ff290a2f7daebb642725e4a8b8451f02ce7
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-07-28 14:55:31 +0300

    Loop until user guesses the number

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,26 @@ use std::cmp::Ordering;
 
 fn main()
 {
-     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_number}");
     println!("Guess the number!\nPlease input your guess");
-    let mut guess = String::new();
-    io::stdin()
-	.read_line(&mut guess)
-	.expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
-    match guess.cmp(&secret_number)
+    loop
     {
-	Ordering::Less => println!("Too small!"),
-	Ordering::Greater => println!("Too Big!"),
-	Ordering::Equal => println!("You win!")
+	let mut guess = String::new();
+	io::stdin()
+	    .read_line(&mut guess)
+	    .expect("Failed to read line");
+	let guess: u32 = guess.trim().parse().expect("Please type a number!");
+	println!("You guessed: {guess}");
+	match guess.cmp(&secret_number)
+	{
+	    Ordering::Less => println!("Too small!"),
+	    Ordering::Greater => println!("Too Big!"),	    
+	    Ordering::Equal =>
+	    {
+		println!("You win!");
+		break;
+	    }
+	}
     }
 }
