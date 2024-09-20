5464ce200fd0e2b45af4ab7279d96ca7220b7d99
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-09-20 12:19:52 +0000

    Add Doc comment to rock-paper-scissors

--- rock-paper-scissors/src/main.rs
+++ rock-paper-scissors/src/main.rs
@@ -1,3 +1,36 @@
+//! > Rock Paper Scissors written in Rust
+//! # Guide
+//! firstly run this crate via cargo:
+//! ```sh
+//! cargo run --package rock-paper-scissors
+//! ```
+//! then it will prompt you for play mode, whether singleplayer or multiplayer:
+//! ```sh
+//! Rock Paper Scissors by Ahmad
+//! choose play mode:
+//! 1. singleplayer
+//! 2. multiplayer
+//! input: 
+//! ```
+//! Type `1` for singleplayer or `2` for multiplayer.
+//! //! If you chose multiplayer it will prompt you for player names:
+//! ```sh
+//! player: 
+//! ```
+//! then it will prompt you for max points number:
+//! ```sh
+//! Enter max points number: 
+//! ```
+//! Supposing you typed `3` it will continue to run the game & prompt each turn for player's choice until someone gets `3` points.
+//! this are valid choices:
+//! | Choice | Alias |
+//! | -------- | ------- |
+//! | `rock` | `r` |
+//! | `paper` | `p` |
+//! | `scissors` | `s` |
+
+
+
 pub use rock_paper_scissors::{choice::Choice, game::Game, mode::Mode, util::mreadln};
 
 fn main() {
